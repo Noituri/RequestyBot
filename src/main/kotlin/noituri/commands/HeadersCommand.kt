98034330f0ae0277536a42f684318f67a3f74d92
noituri <yknomeh@protonmail.com>
2019-05-23 14:00:24 +0200

    commands: header

--- src/main/kotlin/noituri/commands/HeadersCommand.kt
+++ src/main/kotlin/noituri/commands/HeadersCommand.kt
@@ -1,9 +1,80 @@
 package noituri.commands
 
+import net.dv8tion.jda.api.entities.EmbedType
+import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import noituri.BotsConfig
+import noituri.Utils
+import java.time.OffsetDateTime
 
-class HeadersCommand : Command(commandName = "headers", desc = "Sets or gets headers") {
+class HeadersCommand : Command(commandName = "header", desc = "Sets or gets headers") {
     override fun execute(event: MessageReceivedEvent, vararg args: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        if (args.size > 1) {
+            headers[event.author.id] = args.joinToString("").removePrefix(BotsConfig.config[BotsConfig.Companion.BotSpec.botPrefix] + "header")
+            event.channel.sendMessage(MessageEmbed(
+                    null,
+                    "Header",
+                    "Successfully updated",
+                    EmbedType.IMAGE,
+                    OffsetDateTime.now(),
+                    Utils.getStatusColor(200),
+                    null,
+                    null,
+                    null,
+                    null,
+                    MessageEmbed.Footer(
+                            "Issued by ${event.author.name}",
+                            event.author.avatarUrl,
+                            null
+                    ),
+                    null,
+                    null
+            )).queue()
+        } else {
+            val p = headers[event.author.id]
+
+            if (p == null) {
+                event.channel.sendMessage(MessageEmbed(
+                        null,
+                        "Error",
+                        "Header does not exist",
+                        EmbedType.IMAGE,
+                        OffsetDateTime.now(),
+                        Utils.getStatusColor(404),
+                        null,
+                        null,
+                        null,
+                        null,
+                        MessageEmbed.Footer(
+                                "Issued by ${event.author.name}",
+                                event.author.avatarUrl,
+                                null
+                        ),
+                        null,
+                        null
+                )).queue()
+
+                return
+            }
+
+            event.channel.sendMessage(MessageEmbed(
+                    null,
+                    "Header",
+                    "```$p```",
+                    EmbedType.IMAGE,
+                    OffsetDateTime.now(),
+                    Utils.getStatusColor(200),
+                    null,
+                    null,
+                    null,
+                    null,
+                    MessageEmbed.Footer(
+                            "Issued by ${event.author.name}",
+                            event.author.avatarUrl,
+                            null
+                    ),
+                    null,
+                    null
+            )).queue()
+        }    }
 }
